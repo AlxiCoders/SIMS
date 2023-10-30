cddf87f7b13da6ae2b6b5508e49d64ea7d8ae101
Sujan Karna <karna.sujan52@gmail.com>
2023-10-30 17:27:46 +0545

    DbContext configured

---
+++ SIMS/SIMS/Server/Data/Migrations/20231030114015_Dbcontect_Configure.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIMS.Server.Data.Migrations
+{
+    public partial class Dbcontect_Configure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
